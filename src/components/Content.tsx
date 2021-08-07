d24ffdf629557df80cef865d83202eb914721fce
André Torquato <andretorquato@alu.ufc.br>
2021-08-07 18:46:03 -0300

    :zap:perf: applying memo to component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import { MovieCard } from "./MovieCard";
 
 interface MovieProps {
@@ -11,26 +12,36 @@ interface MovieProps {
   Runtime: string;
 }
 
-interface ContentProps{
+interface ContentProps {
   movies: MovieProps[];
   selectedGenreTitle: String;
 }
-export function Content({ movies, selectedGenreTitle }: ContentProps) {
-  // Complete aqui
-
+ const ContentComponent = ({ movies, selectedGenreTitle }: ContentProps) => {
   return (
     <div className="container">
-    <header>
-      <span className="category">Categoria:<span> {selectedGenreTitle}</span></span>
-    </header>
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenreTitle}</span>
+        </span>
+      </header>
 
-    <main>
-      <div className="movies-list">
-        {movies.map(movie => (
-          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-        ))}
-      </div>
-    </main>
-  </div>
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
   );
 }
+
+export const Content = memo(ContentComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps.selectedGenreTitle, nextProps.selectedGenreTitle);
+});
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,39 +1,41 @@
 import { useEffect, useState } from "react";
-import { api } from '../services/api';
+import { api } from "../services/api";
 import { Button } from "./Button";
 
 interface GenreResponseProps {
   id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
   title: string;
 }
 
-interface SideBarProps{
+interface SideBarProps {
   genres: GenreResponseProps[];
   selectedGenreId: Number;
   selectGenreId: (id: number) => void;
 }
-export function SideBar({ genres , selectedGenreId, selectGenreId }: SideBarProps) {
-  
-
+export function SideBar({
+  genres,
+  selectedGenreId,
+  selectGenreId,
+}: SideBarProps) {
   return (
     <nav className="sidebar">
-    <span>Watch<p>Me</p></span>
+      <span>
+        Watch<p>Me</p>
+      </span>
 
-    <div className="buttons-container">
-      {genres.map(genre => (
-        <Button
-          key={String(genre.id)}
-          title={genre.title}
-          iconName={genre.name}
-          // onClick={() => handleClickButton(genre.id)}
-          onClick={() => selectGenreId(genre.id)}
-          selected={selectedGenreId === genre.id}
-        />
-      ))}
-    </div>
-
-  </nav>
-
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            // onClick={() => handleClickButton(genre.id)}
+            onClick={() => selectGenreId(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
   );
 }
